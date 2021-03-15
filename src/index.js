ae62d37d736f4ce12ecc1ee4c524c802c29c9876
laiker <laikernest@gmail.com>
2021-03-15 14:53:58 +0700

    feat: brackets task released

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var newstr = str;
+  for (i = 0; i < Math.floor(str.length / 2); i++) {
+    for (j = 0; j <= bracketsConfig.length - 1; j++) {
+      strPair = bracketsConfig[j].join('');
+      newstr = newstr.replace(strPair, '');
+    }
+  }
+  return newstr.length == 0;
 }
